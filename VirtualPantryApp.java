7dafa8ac495d7a75b2f10af15f22e0ab6ea6b319
knives-knife <93448980+knives-knife@users.noreply.github.com>
2021-11-05 13:54:32 -0500

    My Work
    This is what I've done so far

--- VirtualPantryApp.java
+++ VirtualPantryApp.java
@@ -2,6 +2,10 @@ public class VirtualPantryApp
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LogInGUI().setVisible(true);
+            }
+        });
     }
 }
