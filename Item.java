19940958a1d429a9cd089bfe85e9c559ce278b8c
jorgeren <93563540+jorgeren@users.noreply.github.com>
2021-11-06 16:42:40 -0500

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,45 @@
+
+
+
+
+public class Item {
+
+    String name;
+    int quantity;
+    String expDate;
+    boolean favorite;
+
+    public String getexpDate() {
+        return expDate;
+    }
+
+    public void setexpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
