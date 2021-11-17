3584354b1a066f6d69323e5438b1d671915298c4
knives-knife <93448980+knives-knife@users.noreply.github.com>
2021-11-16 23:08:07 -0600

    Update Item.java

--- Item.java
+++ Item.java
@@ -9,85 +9,101 @@ Item
         a favorite - a boolean that is true for favorite and false otherwise. 
     Methods - 
         All methods are getters or setters for the describes attributes. 
-*/
-
+ */
 
 import java.time.LocalDate;
 
-public class Item {
+public class Item
+{
 
     String name;
     int quantity;
     LocalDate expDate;
     boolean favorite;
 
-    public Item() {
+    public Item()
+    {
         this.name = "initalfoodname";
         this.quantity = 0;
         this.favorite = false;
     }
 
-    public Item(String s, int q, String d){
+    public Item(String s, int q, String d)
+    {
         this.name = s;
         this.quantity = q;
 
-        String[] chars = d.split("");
-        String temp = chars[0]+chars[1]+chars[2]+chars[3]+"-"+chars[4]+chars[5]+"-"+chars[6]+chars[7];
-        expDate = LocalDate.parse(temp);
+        if (d != null)
+        {
+            String[] chars = d.split("");
+            String temp = chars[0] + chars[1] + chars[2] + chars[3] + "-" + chars[4] + chars[5] + "-" + chars[6] + chars[7];
+            expDate = LocalDate.parse(temp);
+        }
         favorite = false;
     }
 
-    public Item(String s){
+    public Item(String s)
+    {
         this.name = s;
         this.quantity = 999;        //if the value is 999, it never runs out?
         this.expDate = null;
     }
 
-
-    public LocalDate getExpDate() {
+    public LocalDate getExpDate()
+    {
         return expDate;
     }
 
-    public void setExpDate(String expD) {
+    public void setExpDate(String expD)
+    {
         String[] chars = expD.split("");
-        String temp = chars[0]+chars[1]+chars[2]+chars[3]+"-"+chars[4]+chars[5]+"-"+chars[6]+chars[7];
+        String temp = chars[0] + chars[1] + chars[2] + chars[3] + "-" + chars[4] + chars[5] + "-" + chars[6] + chars[7];
         this.expDate = LocalDate.parse(temp);
     }
 
-    public void setExpDate(LocalDate expD) {
+    public void setExpDate(LocalDate expD)
+    {
         this.expDate = expD;
     }
-      
-    public LocalDate getexpDate() {
+
+    public LocalDate getexpDate()
+    {
         return expDate;
     }
 
-    public void setexpDate(LocalDate expDate) {
+    public void setexpDate(LocalDate expDate)
+    {
 
         this.expDate = expDate;
     }
 
-    public Boolean getFavorite() {
+    public Boolean getFavorite()
+    {
         return favorite;
     }
 
-    public void setFavorite(boolean favorite) {
+    public void setFavorite(boolean favorite)
+    {
         this.favorite = favorite;
     }
 
-    public int getQuantity() {
+    public int getQuantity()
+    {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(int quantity)
+    {
         this.quantity = quantity;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
